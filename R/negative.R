bee6129d22b2c290367576e0da5d2b3eb1d563f5
christophergandrud <christopher.gandrud@gmail.com>
2016-06-09 15:52:04 +0200

    btscs port working

---
+++ R/negative.R
@@ -0,0 +1,9 @@
+#' Simulated data set from Williams (2016) to illustrate negative duration
+#' dependence
+#' @docType data
+#' @source Williams, Laron K. 2016. "Long-Term Effects in Models with Temporal
+#' Dependence." Political Analysis: 1-20.
+#' @keywords datasets
+#' @format A data set with 1000 observations and 6 variables
+#' @name negative
+NULL
