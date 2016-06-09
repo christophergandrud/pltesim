8f196efcfa4e5894baccccc1ab587fe9ddebddd6
christophergandrud <christopher.gandrud@gmail.com>
2016-06-09 16:53:00 +0200

    added travis, readme and basic tests

---
+++ tests/testthat/test.R
@@ -0,0 +1,9 @@
+data('negative')
+
+neg_df <- btscs(negative, 'y', 'tim', 'group')
+
+test_that('btscs output validity', {
+          expect_equal(nrow(neg_df), 1000)
+          expect_equal(ncol(neg_df), 7)
+          expect_equal(sum(neg_df[['spell']]), 9327)
+})
